de903e8d08265bf419618dcbc820ad7d5c67b60e
maccimac <macci.web@gmail.com>
2021-10-15 12:15:22 +0800

    Create simple express host

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// NOTE: https://expressjs.com/en/starter/hello-world.html
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
